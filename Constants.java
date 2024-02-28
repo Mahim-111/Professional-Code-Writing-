4f7d4eab6e1b3a4d648fab6303ba8c731a6fb757
Md. Mahim Babu <127214556+Mahim-111@users.noreply.github.com>
2024-02-28 11:52:07 +0600

    Add Constants Class

---
+++ Constants.java
@@ -0,0 +1,21 @@
+public class Constants {
+
+    // Name of file containing list of students.
+    public static final String StudentList = "students.txt";
+
+    // Command line argument to show all students.
+    public static final String ShowAll = "-a";
+
+    // Command line argument to show a Random student from the list.
+    public static final String ShowRandom = "-r";
+
+    // Command line argument to display a count of the number of students list.
+    public static final String ShowCount = "-c";
+
+    // Command line argument to find a student in the list.
+    public static final String FindEntry = "?";
+
+    // Command line argument to add an entry to the list.
+    public static final String AddEntry = "+";
+
+}
